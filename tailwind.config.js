2af431cefb087741d3e2f9cb6a7a75c16980294b
Mohamed <mohamedmostafakhudari@gmail.com>
2023-04-09 22:31:07 +0200

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        violet: {
+          dark: "hsl(256, 26%, 20%)",
+          veryDark: "hsl(270, 9%, 17%)",
+          darkGrayish: "hsl(273, 4%, 51%)",
+        },
+        blue: {
+          grayish: "hsl(216, 30%, 68%)",
+        },
+        gray: "hsl(0, 0%, 98%)",
+      },
+      fontFamily: {
+        body: ["'Karla'", "sans-serif"],
+        heading: ["'DM Serif Display'", "serif"],
+      },
+    },
   },
   plugins: [],
-}
-
+};
